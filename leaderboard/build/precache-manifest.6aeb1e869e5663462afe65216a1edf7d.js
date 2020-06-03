068c346e69406f10fbd4f7d3180303a1c73cd1a1
garvitchittora <garvitchittora2016@gmail.com>
2020-06-03 13:40:10 +0530

    build added

---
+++ leaderboard/build/precache-manifest.6aeb1e869e5663462afe65216a1edf7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "249f87012c7cdc5e894b7054e6aaac1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2eb1a0d5b5189b66d65a",
+    "url": "/static/css/2.42ec2a3e.chunk.css"
+  },
+  {
+    "revision": "eac37a244c251615c9ad",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2eb1a0d5b5189b66d65a",
+    "url": "/static/js/2.2a737297.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2a737297.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eac37a244c251615c9ad",
+    "url": "/static/js/main.650c27b9.chunk.js"
+  },
+  {
+    "revision": "5132ffb66f2a6c77677c",
+    "url": "/static/js/runtime-main.1c049339.js"
+  }
+]);
